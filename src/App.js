8e1e427a291555b7ec490043d344c2f47508cc8d
Vinh Nguyen <vinhsnguyen86@gmail.com>
2025-07-14 20:20:15 -0700

    At step 4 of the challenge

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+
+function Flag() {
+  const [flag, setFlag] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const url = 'https://wgg522pwivhvi5gqsn675gth3q0otdja.lambda-url.us-east-1.on.aws/6f7665';
+
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Bad network response');
+        }
+        return response.text();
+      })
+      .then((data) => {
+        setFlag(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching:', error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return <div>{flag}</div>;
+
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Flag />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <img src="https://wgg522pwivhvi5gqsn675gth3q0otdja.lambda-url.us-east-1.on.aws/6f7665" className="App-logo" alt="logo" /> */}
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -16,7 +52,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
